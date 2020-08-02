f67367d4ed621cdf713dd7eab9b37e68bd6c2924
Maanav Singh <50304418+MaanavS16@users.noreply.github.com>
2020-08-02 14:08:31 -0400

    updated module with climate API

--- predictModule.py
+++ predictModule.py
@@ -1,15 +1,27 @@
 import tensorflow as tf
 from baseAlgorithm import predictDisease, diseases
+from climateAPI import Climate
+from dotenv import load_dotenv
 import numpy
 
 class predictor:
     def __init__(self):
+        load_dotenv('.env')
+        usernameM = os.getenv('usernameM')
+        passwordM = os.getenv('passwordM')
+        token = os.getenv('token')
+        openWeatherToken = os.getenv('openWeatherToken')
+        weatherbitToken = os.getenv('weatherBitToken')
+
         self.model = tf.keras.models.load_model('predictiveModel.h5')
+        self.climateLoader = Climate(usernameM, passwordM, token, openWeatherToken, weatherbitToken)
 
     def algorithmPredict(self, climateData, patientData):
         return predictDisease(climateData, patientData)
 
-    def mlPredict(self, climateData, patientData):
+    def mlPredict(self, climateData, patientData, reload=False):
+        if reload:
+            self.model = tf.keras.models.load_model('predictiveModel.h5')
         climate = np.array([climateData])
         patient = np.array([patientData])
         return np.round(model.predict([climate, patient])[0], 2)
@@ -20,4 +32,7 @@ class predictor:
         output = np.array([correctOutput])
         self.model.fit([climate, patient], output, batch_size=1, epochs=5)
         if save:
-            self.model.save('predictModule.h5')
+            self.model.save('predictiveModul.h5')
+
+    def getClimateData(self, long, lat):
+        return self.climateLoader.getAllMetrics(long, lat)

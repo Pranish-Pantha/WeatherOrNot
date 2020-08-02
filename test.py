38c904d716e33e40b33b2345062754eb26006a08
Maanav Singh <50304418+MaanavS16@users.noreply.github.com>
2020-08-02 13:51:07 -0400

    moved files

--- test.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from baseAlgorithm import genClimateData, diseases
-
-climate = genClimateData()
-climate = np.array(list(map(lambda x: climate.get(x), climate.keys())))
-patient = np.array(list(map(lambda x: 0, diseases)))
-
-model = tf.keras.models.load_model('predictiveModel.h5')
-model.predict([climate, patient])
